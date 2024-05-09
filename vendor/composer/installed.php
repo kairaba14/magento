a8647dc16d9ecd0f074f2c7cc58f3d81797a43dd
Kairaba <kairabatour@gmail.com>
2024-05-09 22:27:42 +0200

    Ma branche principale

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firegento/fastsimpleimport' => array(
+            'pretty_version' => '1.3.4',
+            'version' => '1.3.4.0',
+            'reference' => '462127fc59e986d31e7841200bc6582588589612',
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../firegento/fastsimpleimport',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
